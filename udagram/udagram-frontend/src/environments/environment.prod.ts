4173e8ed305a88359daf2f5aecf0eed44c5c54cc
Sylum <106944410+ysakkaan20@users.noreply.github.com>
2023-01-24 19:18:36 +0200

    final edit

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udagram-api-dev222222222222222.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
